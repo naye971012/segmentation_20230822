d577099c4760972c337c7eafc6f38c5b8af6feda
naye971012 <naye03@naver.com>
2023-08-22 06:30:34 +0900

    as

--- losses.py
+++ losses.py
@@ -37,8 +37,8 @@ class CrossEntropy(nn.Module):
         ce_loss = -target * torch.log(torch.sigmoid(score) + 1e-8 ) - (1 - target) * torch.log(1 - torch.sigmoid(score) + 1e-8)
         
         weight = torch.ones_like(ce_loss)
-        weight[:,0,:,:]=0.0
-        weight[:,21,:,:]=0.0
+        weight[:,0,:,:]=0.001
+        weight[:,21,:,:]=0.001
         
         loss = (ce_loss * weight) .mean()
 
